f5d2c913e7b9681dab2763cd31847a215725524b
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-04-02 09:33:57 +0800

    Redo the modalwindow i kinda get confus so restart it

--- src/modal-window.js
+++ src/modal-window.js
@@ -1,13 +1,3 @@
 const modalWindow = (function () {
-  let bgBlurContainer = document.querySelector(".blur");
-  let addBookBtn = document.querySelector(".add__btn");
-
-  addBookBtn.addEventListener("click", blurBg);
-
-  function blurBg() {
-    bgBlurContainer.classList.add("bg__blur");
-    console.log("Clicked");
-  }
-
   
 })();

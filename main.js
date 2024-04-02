1a5856d2cc09c8372925de82140f4102f00cb63f
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-04-02 09:19:41 +0800

    Added blur bg functionality  when addBtn is clicked

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ import "./styles/utility.css";
 import "./styles/compo/main-content.css";
 import "./styles/compo/footer.css";
 import "./styles/compo/header.css";
+import "./styles/compo/modalWindow.css";
 import "./src/modal-window.js";
--- src/modal-window.js
+++ src/modal-window.js
@@ -1,4 +1,13 @@
-const modalWindow = (function() {
+const modalWindow = (function () {
+  let bgBlurContainer = document.querySelector(".blur");
+  let addBookBtn = document.querySelector(".add__btn");
+
+  addBookBtn.addEventListener("click", blurBg);
+
+  function blurBg() {
+    bgBlurContainer.classList.add("bg__blur");
+    console.log("Clicked");
+  }
 
   
 })();

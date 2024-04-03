a430177401ab0161d39e39805b4aa0a1a1230da8
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-04-03 23:02:44 +0800

    Added hide modal when clicked outside the modal window

--- src/modal-window.js
+++ src/modal-window.js
@@ -1,3 +1,19 @@
 const modalWindow = (function () {
-  
+  let addButton = document.querySelector(".add__btn");
+  let modalContainer = document.querySelector(".modal__window");
+
+  addButton.addEventListener("click", showModal);
+  document.addEventListener("click", hideModal);
+
+  function showModal() {
+    modalContainer.style.display = "flex";
+  }
+
+  function hideModal() {
+    modalContainer.addEventListener("click", (e) => {
+      if (!e.target.closest(".window__content")) {
+        modalContainer.style.display = "none";
+      }
+    });
+  }
 })();
